7b480f4e583d8d1915b7ed3454fd5798f87b880c
Brooklyne Finni <brooklyne.finni@gmail.com>
2019-07-27 16:13:32 -0400

    Add files via upload

---
+++ dataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+//determines the value of different data types
+int main()
+{
+   char mychar='A';
+   int myint=15;
+   bool mybool=false;
+   float myfloat=25.63;
+   cout<<"Value in  my char:"<<mychar<<endl;
+   cout<<"Value in my int:"<<myint<<endl;
+   cout<<"Value in my bool:"<<mybool<<endl;
+   cout<<"Value in my float:"<<myfloat<<endl;
+   return 0;
+}
